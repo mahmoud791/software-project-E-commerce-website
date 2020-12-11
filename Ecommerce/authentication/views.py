ad16b009c74cc23cf0af6a42c2516115bec72a61
MoAbdulrahem <1601227@eng.asu.edu.eg>
2020-12-11 23:38:40 +0200

    created model for user

--- Ecommerce/authentication/views.py
+++ Ecommerce/authentication/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from authentication.models import user
 
 def login (request):
-    context = {}
+    context = {
+        'user_name' : user.user_name,
+
+    }
     return render(request,'login/login.html',context)
 
 def register (request):
