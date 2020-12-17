c506abe53e6e1fb38a2e34803492aa053d29ca6c
Tabakh <meltabakh98@gmail.com>
2020-12-17 14:00:50 +0200

    Add Backend Of Seller form

--- Ecommerce/sellerProfile/views.py
+++ Ecommerce/sellerProfile/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from home.models import Product
 # Create your views here.
 
 def sellerProfile (request):
     return render(request,"sellerprofile/sellerprofile.html")
+
+
+def addNewProduct (request):
+
+    name=request.POST["name"]
+    price=request.POST["price"]
+    
+    category=request.POST["category"]
+    description=request.POST["description"]
+    image=request.FILES["image"]
+
+    newproduct = Product(name=name,description=description,price=price,image=image, category= category)
+
+    newproduct.save()
+    return redirect("/sellerprofile")
