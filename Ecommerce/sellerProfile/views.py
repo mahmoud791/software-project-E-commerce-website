7569622671781608837c21fbb1a1cab42396159e
Tabakh <meltabakh98@gmail.com>
2020-12-17 16:15:21 +0200

    Add Profile Layout

--- Ecommerce/sellerProfile/views.py
+++ Ecommerce/sellerProfile/views.py
@@ -11,12 +11,10 @@ def addNewProduct (request):
 
     name=request.POST["name"]
     price=request.POST["price"]
-    
     category=request.POST["category"]
     description=request.POST["description"]
     image=request.FILES["image"]
-
     newproduct = Product(name=name,description=description,price=price,image=image, category= category)
-
     newproduct.save()
+    
     return redirect("/sellerprofile")
